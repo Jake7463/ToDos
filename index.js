1acbcd858d4be498a11afc26814039dd963dd06a
Jake7463 <jacobhick@gmail.com>
2024-02-11 03:42:53 +0200

    Got some JS almost working

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const taskName = document.getElementById("taskname");
-
 function newElement(t){
     //gets the input element and adds it to the list
+    const span = document.createElement("span");
+    const rep = document.getElementById("replist")
+    const checkbox = document.createElement("input")
+    const p = document.createElement("p");
+    span.className = "represent";
+    checkbox.setAttribute("type", "checkbox");
+    checkbox.setAttribute("class","checkbox");
+    p.textContent = t;
+    p.className = "taskname";
+    rep.appendChild(span);
+    span.appendChild(checkbox);
+    span.appendChild(p);
 };
 
 function handleClick(){
     //invokes newElement with the input submitted
+    const taskName = document.getElementById("taskname");
+    newElement(taskName);
 };
 
